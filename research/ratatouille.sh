a78df5b1d9012b60e831f257f3724e94ae780303
RuskinManku <ruskincoder@gmail.com>
2020-01-19 18:13:17 +0530

    Added training script for stagewise neural network

--- research/ratatouille.sh
+++ research/ratatouille.sh
@@ -5,6 +5,7 @@ MODEL_NUM=$4
 PIPELINE_CONFIG_PATH=$5
 NUM_TRAIN_STEPS=$6
 USR_NAME=$7
+TWO_STAGE_CLASSIFICATION=$8
 NUM_SHARDS=1
 PROJECT_DIR="RATATOUILLE"
 DETECTION_MODELS_DIR="${PROJECT_DIR}/DETECTION_MODELS"
@@ -61,26 +62,35 @@ command sed -i "s/bhadwa/${USR_NAME}/g" "${PIPELINE_CONFIG_PATH}"
 echo "Config File Copied" | tee -a $LOG_FILE
 
 echo "Training started." | tee -a $LOG_FILE
-SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-command python3 object_detection/model_main.py \
-    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-    --model_dir=${TRAIN_DIR} \
-    --num_train_steps=${NUM_TRAIN_STEPS} \
-    --sample_1_of_n_eval_examples=$SAMPLE_1_OF_N_EVAL_EXAMPLES \
-    --alsologtostderr
+# SAMPLE_1_OF_N_EVAL_EXAMPLES=1
+# command python3 object_detection/model_main.py \
+#     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+#     --model_dir=${TRAIN_DIR} \
+#     --num_train_steps=${NUM_TRAIN_STEPS} \
+#     --sample_1_of_n_eval_examples=$SAMPLE_1_OF_N_EVAL_EXAMPLES \
+#     --alsologtostderr
 echo "Training ended." | tee -a $LOG_FILE
 
 echo "Exporting Inference Graph" | tee -a $LOG_FILE
-INFERENCE_GRAPH_PATH="${TRAIN_DIR}/inference_graph"
-command mkdir $INFERENCE_GRAPH_PATH
-command python3 object_detection/export_inference_graph.py \
-    --input_type image_tensor \
-    --pipeline_config_path $PIPELINE_CONFIG_PATH \
-    --trained_checkpoint_prefix "${TRAIN_DIR}/model.ckpt-${NUM_TRAIN_STEPS}" path/to/model.ckpt \
-    --output_directory $INFERENCE_GRAPH_PATH
-echo "Inference Graph Exported." | tee -a $LOG_FILE
+INFERENCES_DIR="${USR_DIR}/inferences"
+command mkdir $INFERENCES_DIR
+INFERENCES_GRAPH_PATH="${INFERENCES_DIR}/inference_graph"
+command mkdir $INFERENCES_GRAPH_PATH
+# command python3 object_detection/export_inference_graph.py \
+#     --input_type image_tensor \
+#     --pipeline_config_path $PIPELINE_CONFIG_PATH \
+#     --trained_checkpoint_prefix "${TRAIN_DIR}/model.ckpt-${NUM_TRAIN_STEPS}" path/to/model.ckpt \
+#     --output_directory $INFERENCE_GRAPH_PATH
+# echo "Inference Graph Exported." | tee -a $LOG_FILE
 
 
+if [ $TWO_STAGE_CLASSIFICATION == '1' ] 
+then
+    echo "Beginning stagewise neural network training"
+    NN_OUTPUT_DIR="${INFERENCES_DIR}/nn_weights"
+    command mkdir $NN_OUTPUT_DIR
+    command python object_detection/stage_train.py --images_dir=$IMAGES_DIR --reports_dir=$REPORTS_DIR --output_dir=$NN_OUTPUT_DIR --label_map_path=$LABEL_MAP_PATH
+fi
 
 
 # Redirecting outputs to default
